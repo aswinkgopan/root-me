7bc0210c659ea0f73ffa69cf11750458e6d14e70
François REYNAUD <francois.p.reynaud@gmail.com>
2017-10-04 18:08:17 +0200

    Solve Cryptanalyse/Substitution polyalphabetique - Vigenere

---
+++ cryptanalyse/substitution_polyalphabetique_vigenere/decrypt.py
@@ -0,0 +1,161 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*
+
+encrypted="""Moi Tepdsi Fhrujrlhf
+
+Nu egxex g'vla jmmg ifvgkvq ehcclkk'lgm, p'xgk ihvfshm rrgz pqw whiighyj. "Wptbutsi: Gr nwccxzgqrg tfixai bshk qibti urshfdtamcyr,
+"Tfixzxmxvhb u'nu 'lmgxxf' riyie pr iwitaesi q'nbv uhrcyr"...
+
+Loktuie kblgvl, asgw yxg dxtie.
+
+Qnbg rold hshl, rrgz zaxex djrjlapbzwv xu xdsvl dzxji qx ihhix wvajve
+hvvoragethzjbi pi 1950, hg xfny tqrfx o ixnedhrk zv fvrpi qxfiblvq prl mvne h'gr utqbxy?
+Rq zbng vmlw hshl xrfhme hrfoewl gq uhb z'rohmf jnbh rzpv, cyrezvl msdgrl z'rohmqrg fcuxsi?
+Vi fnwj nu lmgxxf, vgavqd qtbj fvr ysaws...
+Cx tmqr rlh lg tszhr jiz vvqyiavs rolg x'iphzv... Cl wgmf izll hwfypbslq xyq pn izlihvf hrl olmyie iayoemz, pqw phbexymqw dn'wcl t'ebtexbexux yi ytgjxux...
+
+Vi fnwj tb gapyxuv hb eg plvsv. C'hm qgbnhv elw bvbysjllydw rqdcbxyqv chii eh ugmaswvfl jamf vcdflrf vrwizkl yzi skotmpsz.
+Nr e'oz vvqbvvl. "Bfg Tqq Hhuczl, qi zi cxio ihw ysamfvk tsz xetjrbs. Nq p'nb trba hmrf fo kxai"
+Eegtbv zvweif. Bz c't jidxnbbvflrf gbiwv. Mvye prl avflw.
+
+V'ev yozm brq hrvclolvfi nnxfnyh'tyv. C'oz mysgzr nb fkkmzegxii.
+Taxqrql iex tmzygx, q'vla gasy. Vo wtpx oi dns ax cigb. Fb ço wtpx grr xfixbv, o'ifm drkji cyr cs dx zyuw ceoeml.
+Tmwctftx xy'upax a'rbti bef...
+Gw gtygq uh'bz jx zizx zxbrvl tmv zhw...
+Eb wedgr ji'ze wizwr jiv cl wgmf iskba jupbnl...
+Eb wedgr ji'ze u'euqr ioj xuwqmtgsi xa ug'my gs uxcvmmg ioj xavq pn...
+Loktuie kblgvl. Asgx px el'bs jmmg v'sjm qsgie. Mcll sie qrfsj.
+
+Xa exsel q'vla edvvos... lgl tavgx g'vla sgzrkhv lbv xi zhbux...
+Zi bvrvwgbaezx n mfrolve pn ewxgl xqprivfgpugi phadx ki x'lrkczgl hmrf esj olmzif w'ie tjgds, hgs zfwyxwvhb velgfvbgwhnl iex rgjfrli,
+Ar exqyxygti hg fvybkq e y'bbthttqxrgqv jbsfmqbsegl... yz wrkjvny iex gkclol.
+
+Zayf ocll yibigxn hnl rayf lcdflw fshl drklmxw...
+Bg o vml rayekw r eh tqxvms tnppxiex rv uvyrjr iclk iini n e'sthsi cyngr fg hzmmg yozf k'yz wgxob...
+Elw rvnzavgaw pi iboewl ugi y'hb ehbw m pnbgjx lxmmrgh gkl-qmguxg vm zezw thûh.
+Fg h ifi qhazgl tmv qxg jtkmcyrl cl bnravr ioi wlw mtnmvzjbie.
+Prl gvnsw cyv tjrblrf hrl qyhzie e ahij twtdiawfv mysgzrksem kie iyxjvl csxsamozklw, yevl qvne gu wbgh thtqq hrl ufnaxqw qtbj el hqwfxfk.
+
+V'lwf rbmfv fvrpi ztwemlrmrg... Es dhuhq hr e'scxjxdsa xh ux s'mzxrkfliaigv, yt pvtbxq hh uolw. Usgw hmwcbzszw hg gvkcmoi qxxr xemexngh, Jtuw belxf tx xyu tbnfitpx qxex pfg tedgux gz vl r'qxnbh gtz pm tehdiblxq hr zzfnaszw ckcwbaigvf, xh mhbw zshl ogilpqd pkwdbuixw.
+Ahij xetxsehbj... xa zayf gcll htbiyxn tkpqurreg.
+Ehbw dipasivoszw yt qfgueuwftbtx... lx hshl bfnz ebtresq vymymaxzj.
+Gvye ikbgkhuw eeal qfnsigv qx dvtb, wmrf gokbvrmpvms, jtuw pstfs ixsmsmrnl... vm csgw ahij twtqprs qibtmziyl.
+Jfnz garfmflbzil hrl pffiie eghazjbie, zbng wbuezgrs zvl nyqvexg,
+Mhbw zi cnbzlzil tnl zvl wefvbgg ux se yesbo rne vuguxg rovgmxf,
+Ocll hweeflwexg if xebqyxg, zayf foebwyxim xh ehbw yiamsq xu iewnroem ki zshl trbyi ovbbfv jbi o'ifm dfny raxex dihwvq fvxb vmyi, qx ahij lvqyif xbthyi pif vfzfprqpf.
+
+Hiz, cl wgmf nb tkpqurre. Afg jvuqr xgk vlpgm qx zr vbvusfbhv.
+Fvr ovvfs vla gqphb rv cbkqv yxg xxuw bee vs hn'ppe trggvga if hvls, gtz wqpbg zvny ebtnksevl.
+Qar pkwdx lwf hr ocll zydtnlgvk, xyqpdns tavwq uhx jfnz rq qr ioiwvrziexn atteuw.
+
+Wx glbz yz lnvyvk, lx oipb sjm tsz qngwwxzxq.
+Zbng ghbzqd nkfvmlv oig bbubcmpy, ztwj ovye rr iclold bef mcll usgw nkfvmlv...
+Mtexg khbx, zshl gfftie xbng cxz qqqrl."""
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+def encrypt_char(p, k):
+	if p.lower() not in alphabet:
+		return p
+	pIndex = alphabet.index(p.lower())
+	kIndex = alphabet.index(k.lower())
+	if p in alphabet:
+		return alphabet[(pIndex + kIndex)%26]
+	else:
+		return alphabet[(pIndex + kIndex)%26].upper()
+
+def decrypt_char(c, k):
+	if c.lower() not in alphabet:
+		return c;
+	cIndex = alphabet.index(c.lower())
+	kIndex = alphabet.index(k.lower())
+	if c in alphabet:
+		return alphabet[(cIndex - kIndex)%26]
+	else:
+		return alphabet[(cIndex - kIndex)%26].upper()
+
+def get_key_char(p, c):
+	if c.lower() not in alphabet or p.lower() not in alphabet:
+		return ''
+
+	cIndex = alphabet.index(c.lower())
+	pIndex = alphabet.index(p.lower())
+	k = (cIndex - pIndex)%26
+	return alphabet[k]
+
+def encrypt(plain, key):
+	i = 0
+	n = len(key)
+	encrypted = ''
+
+	for p in plain:
+		if i == n:
+			i = 0
+		encrypted += encrypt_char(p, key[i])
+		if p.lower() in alphabet:
+			i += 1
+
+	return encrypted
+
+def decrypt(encrypted, key):
+	i = 0
+	n = len(key)
+	plain = ''
+
+	for p in encrypted:
+		if i == n:
+			i = 0
+		plain += decrypt_char(p, key[i])
+		if p.lower() in alphabet:
+			i += 1
+
+	return plain
+
+#aujourd'hui
+#pfxdjcr'wjt
+#plopplo pl
+#aujourd hui
+
+def find_subkey(guess, encrypted):
+	n = len(encrypted)
+	m = len(guess)
+	i = 0
+	ret = []
+	skip = 0
+	while i+skip < n-m:
+		acc = ''
+		j = 0
+		l = 0
+		skip = 0
+		while l < m and i+l+skip < n:
+			k = get_key_char(guess[j], encrypted[i+l+skip])
+			#print(i+l+skip,guess[j],encrypted[i+l+skip], k)
+			if k == '':
+				skip+=1
+				continue
+			else:
+				j=(j+1)%m
+				acc += k
+			l += 1
+		#print(acc)
+		ret.append(acc)
+		i += 1
+	return ret
+
+
+import sys
+guess = "aujourdhui"
+guess = sys.argv[1]
+s = encrypted.split('\n')[2]
+
+print(s)
+sys.stdout.flush()
+
+keys = find_subkey(guess, s)
+i = 0
+for k in keys:
+	plain = decrypt(s[i:], k)
+	print(k,plain)
+	i+=1
+
+print(decrypt(encrypted,"thementor"))
